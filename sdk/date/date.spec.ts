6b7784af140fe83786f55a54bfb9db9f6e726838
whj <emailwen@qq.com>
2022-02-14 20:40:53 +0800

    feat: 新增日期相关的操作库

---
+++ sdk/date/date.spec.ts
@@ -0,0 +1,40 @@
+import { date } from "./date"
+
+
+describe('日期测试', () => {
+  let today = new Date()
+  let y = today.getFullYear()
+  let m = today.getMonth() + 1
+  let d = today.getDate()
+
+  it('以默认的匹配器进行日期格式化', () => {
+    let ymd = `${y}-${m < 10 ? '0' + m : m}-${d < 10 ? '0' + d : d}`
+    let ymd2 = `${y}/${m < 10 ? '0' + m : m}/${d < 10 ? '0' + d : d}`
+
+    expect(date().format()).toBe(ymd)
+    expect(date().format('yyyy/MM/dd')).toBe(ymd2)
+    expect(date('2020-01-01').format('yyyy/MM/dd')).toBe('2020/01/01')
+    expect(date('2020-01-01').format('yyyy-M-d hh:m:ss')).toBe('2020-1-1 08:0:00')
+  })
+
+  it('计算相对此刻日期', () => {
+    expect(date('2022-02-14').calc(10, 'days').format()).toBe('2022-02-24')
+    expect(date('2022-02-14').calc(-10, 'days').format()).toBe('2022-02-04')
+    expect(date('2022-02-14').calc(10, 'months').format()).toBe('2022-12-14')
+    expect(date('2022-02-14').calc(10, 'weeks').format()).toBe('2022-04-25')
+    expect(date('2022-02-14').calc(10, 'years').format()).toBe('2032-02-14')
+  })
+
+  it('设置匹配器', () => {
+    date.setMatcher('w+', (date: Date, len: number) => {
+      if (len === 1) {
+        return date.getDay() + ''
+      } else {
+        return '0' + date.getDay()
+      }
+    })
+    expect(date('2022-02-14').format('w')).toBe('1')
+    expect(date('2022-02-14').format('ww')).toBe('01')
+
+  })
+})
--- sdk/date/date.ts
+++ sdk/date/date.ts
@@ -1,30 +1,123 @@
-import { isDate } from 'sdk/utils/data-type'
+import { isDate } from '..'
 
-export class Dater {
+class Dater {
   constructor(date: number | string | Date) {
-    if (isDate(date)) {
+    if (date instanceof Dater) {
+      this.date = date.date
+    } else if (isDate(date)) {
+      this.date = date
+    } else {
+      this.date = new Date(date)
     }
   }
 
-  format(formatter: string) {
-    return ''
+  private date!: Date
+
+  private _timestamp?: number
+
+  private static matchers: Record<string, (date: Date, len: number) => string> = {
+    yyyy: (date: Date) => date.getFullYear() + '',
+    'M+': (date: Date, len: number) => {
+      let month = `${date.getMonth() + 1}`
+      return len === 1 ? month : `0${month}`.slice(-2)
+    },
+    'd+': (date: Date, len: number) => {
+      let day = date.getDate() + ''
+      return len === 1 ? day : `0${day}`.slice(-2)
+    },
+    'h+': (date: Date, len: number) => {
+      let hour = date.getHours()
+      let strHour = (hour > 12 ? hour - 12 : hour) + ''
+      return len === 1 ? strHour : `0${strHour}`.slice(-2)
+    },
+    'H+': (date: Date, len: number) => {
+      let Hour = `${date.getHours()}`
+      return len === 1 ? Hour : `0${Hour}`.slice(-2)
+    },
+    'm+': (date: Date, len: number) => {
+      let mih = `${date.getMinutes()}`
+      return len === 1 ? mih : `0${mih}`.slice(-2)
+    },
+    's+': (date: Date, len: number) => {
+      let sec = `${date.getSeconds()}`
+      return len === 1 ? sec : `0${sec}`.slice(-2)
+    }
   }
 
-  add() {}
+  /** 时间戳 */
+  get timestamp() {
+    if (this._timestamp) return this._timestamp
+    this._timestamp = this.date.getTime()
+    return this._timestamp
+  }
+
+  static setMatcher(reg: string, matcher: (date: Date, len: number) => string) {
+    Dater.matchers[reg] = matcher
+  }
+
+  static use(plugin: (dater: typeof Dater) => void) {
+    plugin(Dater)
+  }
+
+  /** 获取所有的匹配器 */
+  static getMatchers() {
+    return Dater.matchers
+  }
+
+  /** 格式化日期 */
+  format(formatter = 'yyyy-MM-dd') {
+    Object.keys(Dater.matchers).forEach(reg => {
+      formatter = formatter.replace(new RegExp(`(${reg})`), str => {
+        return Dater.matchers[reg](this.date, str.length)
+      })
+    })
+    return formatter
+  }
+
+  /**
+   * 计算相对此刻的日期
+   * @param timeStep 计算的日期, 负数表示之前的日期, 正数表示之后的日期
+   * @param type 时间步长类别, 默认以天为单位
+   */
+  calc(timeStep: number, type?: 'days' | 'weeks' | 'months' | 'years') {
+    const { date } = this
+    if (type === 'days') {
+      return new Dater(this.timestamp + timeStep * 86400000)
+    } else if (type === 'weeks') {
+      return new Dater(this.timestamp + timeStep * 604800000)
+    } else if (type === 'months') {
+      date.setMonth(timeStep + date.getMonth())
+      return new Dater(date.getTime())
+    } else {
+      date.setFullYear(timeStep + date.getFullYear())
+      return new Dater(date.getTime())
+    }
+  }
 }
 
-interface DateUtil {
-  (date: number | string | Date): Dater
-  add(args: Date): void
-  minus(args: Date): void
+interface DateFactory {
+  (date?: number | string | Date): Dater
+  /**
+   * 虽然可以直接操作Dater的api，但可以使用插件机制来更好的组织你的代码
+   * @param plugin 插件
+   */
+  use: (plugin: (dater: typeof Dater) => void) => void
+  /** 获取所有的匹配器 */
+  getMatchers: () => Record<string, (date: Date, len: number) => string>
+  /**
+   * 设置匹配器，你可以新增或者覆盖原本的配器
+   * @param reg 匹配器名称
+   * @param matcher 匹配器
+   */
+  setMatcher: (reg: string, matcher: (date: Date, len: number) => string) => void
 }
 
-export let date = ((date: number | string | Date) => {
-  return new Dater(date)
-}) as DateUtil
-
-date.add = function (args) {
-
+export const date = <DateFactory>function (date) {
+  return new Dater(date ?? new Date())
 }
 
-date.minus = function (args) {}
+date.use = Dater.use
+
+date.getMatchers = Dater.getMatchers
+
+date.setMatcher = Dater.setMatcher
