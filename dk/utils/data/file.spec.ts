832e0c526c4b83b7fa7857ca8cd5e2116f5b4189
snowerskk <914637371@qq.com>
2022-02-28 16:04:31 +0800

    feat:删除金额转大写

--- dk/utils/data/file.spec.ts
+++
@@ -1,8 +0,0 @@
-import { requireImg } from './file'
-
-describe('静态文件测试', () => {
-  //error TS1343: The 'import.meta' meta-property is only allowed when the '--module' option is 'es2020', 'es2022', 'esnext', 'system', 'node12', or 'nodenext'.
-  // it('引入本地静态图片', () => {
-  //   expect(requireImg(['ship', 'train'], 'test', 'png')).toBe('')
-  // })
-})
