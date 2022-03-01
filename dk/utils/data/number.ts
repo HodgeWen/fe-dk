6b86e44015970ef4246d08f6a683aa8d201134b8
snowerskk <914637371@qq.com>
2022-03-01 14:42:02 +0800

    feat: 数字操作-format

--- dk/utils/data/number.ts
+++ dk/utils/data/number.ts
@@ -7,6 +7,69 @@ class Num {
     maximumFractionDigits: 2
   })
 
+  private money(money: number) {
+    if (!money) return '0'
+    const [intPart, decPart] = String(money).split('.')
+    const len = intPart.length - 1
+    let arr: string[] = []
+    intPart
+      .split('')
+      .reverse()
+      .forEach((item: string, index: number) => {
+        arr.push(item)
+        if (index && (index + 1) % 3 === 0 && index !== len) {
+          arr.push(',')
+        }
+      })
+    let result = arr.reverse().join('')
+    if (decPart) result = `${result}.${decPart}`
+    return result
+  }
+
+  private cn_money(money: number) {
+    const CN_NUMS = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖']
+    const CN_INT_RADICE = ['', '拾', '佰', '仟']
+    const CN_INT_UNITS = ['', '万', '亿', '兆']
+    const CN_DEC_UNITS = ['角', '分', '毫', '厘']
+    let result = ''
+    if (!money) return '零元整'
+    if (money >= 999999999999999.9999) return ''
+    const [intPart, decPart] = String(+money.toFixed(4)).split('.')
+    if (parseInt(intPart, 10) > 0) {
+      let count = 0
+      const IntLen = intPart.length
+      for (let i = 0; i < IntLen; i++) {
+        let n = intPart.substring(i, i + 1)
+        let p = IntLen - i - 1
+        let q = p / 4
+        let m = p % 4
+        if (n === '0') {
+          count++
+        } else {
+          if (count > 0) {
+            result += CN_NUMS[0]
+          }
+          count = 0
+          result += CN_NUMS[parseInt(n)] + CN_INT_RADICE[m]
+        }
+        if (m === 0 && count < 4) {
+          result += CN_INT_UNITS[q]
+        }
+      }
+      result = `${result}元`
+    }
+    if (decPart) {
+      const decLen = decPart.length
+      for (let i = 0; i < decLen; i++) {
+        let n = decPart.substring(i, i + 1)
+        if (n !== '0') result += CN_NUMS[Number(n)] + CN_DEC_UNITS[i]
+      }
+    } else {
+      result = `${result}整`
+    }
+    return result
+  }
+
   constructor(n: number) {
     this.v = n
   }
@@ -17,6 +80,7 @@ class Num {
    */
   format(type: FormatType) {
     const { v } = this
+    return type === 'cn-money' ? this.cn_money(v) : this.money(v)
   }
 
   /**
